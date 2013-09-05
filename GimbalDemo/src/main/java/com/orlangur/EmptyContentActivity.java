2c81cc08098e186d82e16a2a217e7bfd837963c1
Piotr Szymanek <pio.szymanek@gmail.com>
2013-09-05 20:42:27 +0200

    Pierwszy commit projektu demo

---
+++ GimbalDemo/src/main/java/com/orlangur/EmptyContentActivity.java
@@ -0,0 +1,15 @@
+package com.orlangur;
+
+import com.orlangur.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmptyContentActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.empty_content);
+    }
+
+}
